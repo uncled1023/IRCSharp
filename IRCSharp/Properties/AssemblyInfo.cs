d4dc45a24cd8bf9a60254e1316193fca3a06c804
Uncled1023 <admin@teknik.io>
2017-03-24 19:18:13 -0700

    Added nuget package creation scripts and removed dependencies

--- IRCSharp/Properties/AssemblyInfo.cs
+++ IRCSharp/Properties/AssemblyInfo.cs
@@ -21,6 +21,8 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c1897794-5242-47cf-a87f-8e76e226869a")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
 // Version information for an assembly consists of the following four values:
 //
